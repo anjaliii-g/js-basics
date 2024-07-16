a694f0db62d27be8e0a68a29bc6bc6894b13da39
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-16 14:53:50 +0000

    datatypes

---
+++ 01-basics/01-variables.js
@@ -0,0 +1,14 @@
+const accountId = 12345                
+let accEmail = "anjali@google.com"
+var accPassword = "123"
+accCity = "hyd" 
+
+//accountId = 2    //error. const cant be changed
+
+accEmail = "asdf@rllt.com"    //let can see block. by default = undefined
+accPassword = "456"          // var is not used frequently. it cannot see block scope.
+accCity = "blr"
+console.log(accEmail)
+console.log(accPassword)
+console.log(accCity)
+console.table([accEmail, accPassword, accCity])
---
+++ 01-basics/02-datatypes.js
@@ -0,0 +1,22 @@
+"use strict" ;   //treat all js code as newer version
+
+//alert("hello")   // used in brower. we are using nodejs
+
+let name = "man"   // string
+let age = 10       //number
+let isOk = true    //bool
+
+
+//number
+//bigInt = very big num
+//string
+//boolean = true/false
+//null =standalone value. empty value. can also be assigned. 
+        //typeof = object
+//undefined = only variable is defined not initialized
+            //typeof = undefined
+//symbol = used for uniqueness
+
+//object
+
+console.log(typeof null);

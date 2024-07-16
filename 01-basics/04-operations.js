3cfac23f48e45ca75a7532363b006083542fcfee
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-16 15:31:16 +0000

    operations

---
+++ 01-basics/04-operations.js
@@ -0,0 +1,16 @@
+let value = 3
+let negval = -value
+console.log(negval)
+
+console.log(2+2)  //+-/*%
+console.log(2**2) // used for power. 2 is to power 2
+
+let str1 = "hi"
+let str2 = " hello"
+console.log(str1+str2);  //strings can aslo be added
+
+console.log("1"+2);  //12
+console.log("1"+2+2 );   //122 not 14
+console.log(2+2+"1");   //41 not 221
+
+console.log(+true);     //1

b64b5c6cb7530be19952b000dfde8bd2eacf6907
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-17 17:45:36 +0000

    num

---
+++ 01-basics/09-numbers.js
@@ -0,0 +1,11 @@
+const num = 200
+console.log(num.toString().length);
+
+
+const num2 = new Number(num)
+console.log(num2);
+//now the num2 is object, we can perform any function like tostring, tofixed
+//toPrecision
+
+//ceil, floor, min, max, round, random
+ 

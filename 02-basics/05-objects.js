6a3300a72c55f4a673af07f0d66ddaaf02300756
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-17 21:44:31 +0000

    destructuring

---
+++ 02-basics/05-objects.js
@@ -0,0 +1,23 @@
+//de structuring
+
+const course={
+    coursename:"js",
+    price:"12",
+    courseins:"hitesh"
+}
+//extract values
+//course.courseins
+
+const {courseins: ins} =course //shorting the name
+console.log(ins);
+
+//json
+// {
+//     "id":"re",
+//     "fd":"fd"
+// }
+
+// [
+//     {},
+//     {}
+// ]

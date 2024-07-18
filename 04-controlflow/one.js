4560e2c59ab920eb858aa66a9d2d806cc8b167c3
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-18 16:30:06 +0000

    if/else

---
+++ 04-controlflow/one.js
@@ -0,0 +1,28 @@
+//if
+
+if(0){
+ console.log("inside if");
+} else{
+    console.log("inside else");
+}
+
+//<,>,==,===,!=, <=,>=
+
+//switch
+
+const month =3;
+switch (month) {
+    case 1:
+        console.log("jan");
+        break;
+    case 2:
+        console.log("feb");
+     break;
+    case 3:
+        console.log("mar");
+    break; 
+
+    default:
+        console.log("idk");
+        break;
+}

8639a771f06916077bba0dd698a9f6dcdd29fb2f
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-18 10:45:38 +0000

    scope

---
+++ 03-basics/03-scopes.js
@@ -0,0 +1,12 @@
+if(true){
+    let a=10 //have local scope
+    const b=20 //local scope
+    var c=30  //no local scope
+}
+// console.log(a);
+// console.log(b);
+// console.log(c); //c is accessed from here also. thats why should not use
+
+//global values are available in scope but scope arent outside the scope
+
+

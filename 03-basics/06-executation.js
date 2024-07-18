e0ef30de0f40a4a9601a8aba34ad688ce059b2e6
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-18 13:27:36 +0000

    exe

---
+++ 03-basics/06-executation.js
@@ -0,0 +1,19 @@
+//js executation context
+
+//js run code in 2 phases
+
+//js is single threaded
+
+//when we run the code, global executation context will create. 
+//"this" variable reffers to this global ec
+
+//func ec
+//eval ec = property of global ec
+
+
+//first = global exeucation phase == this 
+// = memory creation phase
+//sec = executation phase
+
+
+

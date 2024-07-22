9c6d8f905580de3db1aeba80d0baf054724cd84e
anjaliii-g <anjaligupta150198@gmail.com>
2024-07-22 05:12:22 +0000

    mp

---
+++ 05-loops/05-map.js
@@ -0,0 +1,11 @@
+//filter
+mynums = [1,2,3,4,5]
+
+const newnums = mynums.filter((num)=>{return num>=3})
+console.log(newnums);
+
+//map
+const newnums1 = mynums.filter((num)=>{return num+3})
+console.log(newnums);
+
+//reduce
